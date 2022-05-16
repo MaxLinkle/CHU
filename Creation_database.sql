c147a7688165e1d128ed74d148b3196b66380a19
atarte <tarte.antoine@gmail.com>
2022-05-16 23:35:11 +0200

    fix rdv

--- Creation_database.sql
+++ Creation_database.sql
@@ -3,23 +3,23 @@ create database if not exists projet_chu;
 use projet_chu;
 
 -- Creating fact table HosCo
-create table if not exists fact_hosco(
+CREATE EXTERNAL TABLE IF NOT EXISTS fact_hosco(
     id_patient int,
     nb_consultation int,
     nb_hospitalisation int
 )
-comment 'Fact table HosCo'
-row format delimited 
-fields terminated by ';'
-stored as textfile
-location '/user/cloudera/CHU/Fact_HosCo'
+COMMENT 'Fact table Hosco'
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ';'
+STORED AS TEXTFILE
+LOCATION '/user/cloudera/CHU/Fact_HosCo';
 
 -- Creating dimension table RDV
-CREATE TABLE IF NOT EXISTS dim_rdv(
+CREATE EXTERNAL TABLE IF NOT EXISTS dim_rdv(
     num_consultation INT,
     date_rdv date,
-    heure_debut date,
-    heure_fin date
+    heure_debut string,
+    heure_fin string
 )
 COMMENT 'Dim table RDV'
 ROW FORMAT DELIMITED
