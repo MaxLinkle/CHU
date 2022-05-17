7b1e87d35525bca5d72a88a47005b0045719f332
JVBruno <78025429+JVBruno@users.noreply.github.com>
2022-05-17 10:38:33 +0200

    Add files via upload

---
+++ Verification_donnees.sql
@@ -0,0 +1,43 @@
+-- Verification dim_diagnostic
+
+SELECT * FROM projet_chu.dim_diagnostic;
+
+-- Verification dim_etablissement
+
+SELECT * FROM projet_chu.dim_etablissement;
+
+-- Verification dim_patient
+
+SELECT * FROM projet_chu.dim_patient;
+
+-- Verification dim_professionel_sante
+
+SELECT * FROM projet_chu.dim_professionel_sante;
+
+-- Verification dim_rdv
+
+SELECT * FROM projet_chu.dim_rdv;
+
+-- Verification dim_region
+
+SELECT * FROM projet_chu.dim_region;
+
+-- Verification dim_visite
+
+SELECT * FROM projet_chu.dim_visite;
+
+-- Verification dim_deces
+
+SELECT * FROM projet_chu.dim_deces;
+
+-- Verification dim_hosco
+
+SELECT * FROM projet_chu.dim_hosco;
+
+-- Verification dim_hospitalisation
+
+SELECT * FROM projet_chu.dim_hospitalisation;
+
+-- Verification dim_satisfaction
+
+SELECT * FROM projet_chu.dim_satisfaction;
